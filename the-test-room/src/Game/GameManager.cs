ca5dd8a4c968dd7df15329620916c155c2c504b5
zander <zander.riggs@gmail.com>
2024-01-23 19:58:56 -0500

    Adds basic runprogram method and renames src folder

---
+++ the-test-room/src/Game/GameManager.cs
@@ -0,0 +1,19 @@
+﻿namespace the_test_room.Game;
+
+class GameManager
+{
+    public GameManager() { }
+
+    public void RunProgram()
+    {
+        var input = "";
+
+        while (!input.Equals("q", StringComparison.CurrentCultureIgnoreCase))
+        {
+            Console.Clear();
+            Console.WriteLine("The game has started!");
+            Console.WriteLine("Press \"q\" to quit.");
+            input = Console.ReadLine();
+        }
+    }
+}
--- the-test-room/src/Program.cs
+++ the-test-room/src/Program.cs
@@ -1,7 +1,10 @@
-﻿Console.WriteLine("Starting Game...");
+﻿using the_test_room.Game;
+
+Console.WriteLine("Starting Game...");
 
 // Application logic soon to come
-// RunApplication();
+var gameManager = new GameManager();
+gameManager.RunProgram();
 
 // For testing:
 // Keeps the console window open for review
