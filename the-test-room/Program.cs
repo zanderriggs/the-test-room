6f1d1e09d6ceb6fdc3fda7c11609f711097a33fd
zander <zander.riggs@gmail.com>
2024-01-23 23:37:38 -0500

    renamed directory and added simple methods for handling user input

---
+++ the-test-room/Program.cs
@@ -0,0 +1,12 @@
+﻿using the_test_room.Game;
+
+Console.WriteLine("Starting Game...");
+
+// Application logic soon to come
+var gameManager = new GameManager();
+gameManager.RunProgram();
+
+// For testing:
+// Keeps the console window open for review
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
---
+++ the-test-room/the-test-room/Game/GameManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Text.RegularExpressions;
+
+namespace the_test_room.Game;
+
+class GameManager
+{
+    public GameManager() { }
+
+    /// <summary>
+    /// Runs the game and returns when user enters input to quick
+    /// </summary>
+    public void RunProgram()
+    {
+        var input = "";
+
+        while (!input.Equals("q", StringComparison.CurrentCultureIgnoreCase))
+        {
+            Console.Clear();
+            // TODO: method to produce output for user
+                // Output is made up of Decription and Prompt
+            Console.WriteLine("The game has started!");
+            Console.WriteLine("Press \"q\" to quit.");
+
+            // Collect user input
+            input = GetUserInput();
+        }
+    }
+
+    /// <summary>
+    /// Collects user input and ensures that it's alphanumeric.
+    /// </summary>
+    /// <returns>Stringified version of user input.</returns>
+    static string GetUserInput()
+    {
+        var input = Console.ReadLine();
+
+        if (IsAlphanumeric(input))
+        {
+
+        }
+
+        return input;
+    }
+
+    /// <summary>
+    /// Checks whether input is alphanumeric.
+    /// </summary>
+    /// <param name="input"></param>
+    /// <returns>Boolean true/false.</returns>
+    static bool IsAlphanumeric(string input)
+    {
+        // Regular expression pattern to match only letters and numbers
+        string pattern = "^[a-zA-Z0-9]+$";
+
+        // Check if the input matches the pattern
+        return Regex.IsMatch(input, pattern);
+    }
+}
