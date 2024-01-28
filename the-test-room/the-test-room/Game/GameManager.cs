fbab60c83a473ceb641adc4af95e57057df864b4
zander <zander.riggs@gmail.com>
2024-01-28 12:17:51 -0500

    Rewords level loading to account for more locations in a level.

--- the-test-room/the-test-room/Game/GameManager.cs
+++ the-test-room/the-test-room/Game/GameManager.cs
@@ -1,6 +1,8 @@
 ﻿using Newtonsoft.Json;
 using System.Text.RegularExpressions;
 using the_test_room.Game.Models;
+using System.Linq;
+using System.Data;
 
 namespace the_test_room.Game;
 
@@ -22,7 +24,8 @@ class GameManager
                 // Output is made up of Decription and Prompt
             Console.WriteLine("The game has started!");
             Console.WriteLine("You are entering the first room...");
-            var room = GetLocation();
+            var level = LoadLevel();
+            var room = level.Locations.FirstOrDefault(x => x.Id.Equals("1"));
             Console.WriteLine($"Room description: {room.Description}");
             Console.WriteLine("Press \"q\" to quit.");
 
@@ -61,31 +64,35 @@ class GameManager
         return Regex.IsMatch(input, pattern);
     }
 
-    public Location GetLocation()
+    /// <summary>
+    /// Loads the level.
+    /// </summary>
+    /// <returns>Level object with list of locations.</returns>
+    public Hotel LoadLevel()
     {
         // Filepath to asset
-        var jsonFilePath = "C:\\git\\the-test-room\\The-Test-Room\\the-test-room\\Assets\\Locations.json";
+        var jsonFilePath = "C:\\git\\the-test-room\\The-Test-Room\\the-test-room\\Assets\\Hotel.json";
         
         // Read JSONfile at filepath
         var jsonContent = File.ReadAllText(jsonFilePath);
-        var location = new Location();
+        var level = new Hotel();
+
         try
         {
             // Deserialize JSON
-            location = JsonConvert.DeserializeObject<Location>(jsonContent);
-            Console.WriteLine(location);
+            level = JsonConvert.DeserializeObject<Hotel>(jsonContent);
 
-            if (location == null)
+            if (level == null)
             {
                 Console.WriteLine("Location data not found");
-                return new Location();
+                return new Hotel();
             }
-            return location;
+            return level;
         }
         catch (Exception ex) 
         {
             Console.WriteLine($"An exception occurred while deserializing Json: {ex.Message}");
-            return new Location();
+            return new Hotel();
         }
     }
 }
---
+++ the-test-room/the-test-room/Game/Models/Hotel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace the_test_room.Game.Models;
+
+public class Hotel
+{
+    public List<Location> Locations { get; set; }
+
+    // More level information can go here
+}
+
